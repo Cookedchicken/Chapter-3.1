d90b6658f5106208e5a796ea7d8ed87f4e387d27
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2020-01-30 13:02:47 -0600

    bruh

--- src/gamezone/PickTwoCards.java
+++ src/gamezone/PickTwoCards.java
@@ -3,21 +3,25 @@ package gamezone;
 public class PickTwoCards {
     public static void main(String[]args){
         final int CARDS_IN_SUIT = 13;
+        char suit [] = {'s', 'c', 'h', 'd'};
         int myValue;
         int yourValue;
+        int r1 = ((int) (Math.random() * suit.length));
+        int r2 = ((int) (Math.random() * suit.length));
+
         Card myCard = new Card ();
         Card yourCard = new Card();
         myValue = ((int) (Math.random() * 100)% CARDS_IN_SUIT + 1);
-        yourValue = ((int) (Math.random() * 100) % CARDS_IN_SUIT + 2);
+        yourValue = ((int) (Math.random() * 100) % CARDS_IN_SUIT + 1);
 
         myCard.setValue(myValue);
-        myCard.setSuit('s');
+        myCard.setSuit(suit[r1]);
 
         yourCard.setValue(yourValue);
-        yourCard.setSuit();
+        yourCard.setSuit(suit[r2]);
 
         System.out.println("My card is the " + myCard.getValue() + " of " + myCard.getSuit());
-        System.out.println("Your card id the " + yourCard.getValue() + " of " + your );
+        System.out.println("Your card is the " + yourCard.getValue() + " of " + yourCard.setSuit());
     }
 
 }
